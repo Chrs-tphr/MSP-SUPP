6cbd471e698dbf07f277db6940def329df375074
Christopher <cisensano@yahoo.com>
2018-03-13 09:17:27 -0700

    testing

--- SCRIPTS/Events/ASB;MCD!~!~!~.js
+++ SCRIPTS/Events/ASB;MCD!~!~!~.js
@@ -1,6 +1,15 @@
+logDebug("In ASB Event script");
+cancel = true;
+showMessage = true;
+comment("In ASB Event script");
+
+/*
+
 var dupVINCheck = queryConflictVIN();
 if(dupVINCheck.isIssue){
 	cancel = true;
 	showMessage = true;
 	comment(dupVINCheck.issueMessage.join("\n<br>"));
 }
+
+*/
