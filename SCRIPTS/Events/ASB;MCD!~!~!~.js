eec9065e88af5ac28efbcf36260be2eabaeff87f
l3lacknight <35308970+l3lacknight@users.noreply.github.com>
2018-04-03 18:15:25 +0000

    18ACC-296750

--- SCRIPTS/Events/ASB;MCD!~!~!~.js
+++ SCRIPTS/Events/ASB;MCD!~!~!~.js
@@ -1,17 +1,21 @@
-if(publicUser){
-	var pCvedNum = parentCapId.getCustomID();
-	logDebug("pCvedNum: "+pCvedNum);
-	
-	var dupVINCheck = queryConflictVIN(pCvedNum);
-}else{
-	showDebug = true;
-	showMessage = true;
-	logDebug("running asb in AA, checking for duplicate vin's");
-	var dupVINCheck = queryConflictVIN();
-}
+////Commented out for testing purposes
+//if(publicUser){
+//	var pCvedNum = parentCapId.getCustomID();
+//	logDebug("pCvedNum: "+pCvedNum);
+//	
+//	var dupVINCheck = queryConflictVIN(pCvedNum);
+//}else{
+//	showDebug = true;
+//	showMessage = true;
+//	logDebug("running asb in AA, checking for duplicate vin's");
+//	var dupVINCheck = queryConflictVIN();
+//}
+//
+//if(dupVINCheck.isIssue){
+//	cancel = true;
+//	showMessage = true;
+//	comment(dupVINCheck.issueMessage.join("\n<br>"));
+//}
 
-if(dupVINCheck.isIssue){
-	cancel = true;
-	showMessage = true;
-	comment(dupVINCheck.issueMessage.join("\n<br>"));
-}
+
+validateCapLP();
