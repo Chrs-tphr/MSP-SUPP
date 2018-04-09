da8565d2065a4965369557b734b6e42749b6bd2b
Christopher <cisensano@yahoo.com>
2018-04-09 07:08:21 -0700

    changing asb to record specific events

--- SCRIPTS/Events/ASB;MCD!~!~!~.js
+++ SCRIPTS/Events/ASB;MCD!~!~!~.js
@@ -1,26 +1,4 @@
-//Comment out until next comment for testing purposes
-if(publicUser && (appMatch("MCD/*/Application/NA") || appMatch("MCD/*/Equipment List/NA"))){
-	//do nothing
-}else if(publicUser){
-	var pCvedNum = parentCapId.getCustomID();
-	logDebug("pCvedNum: "+pCvedNum);
-	
-	var dupVINCheck = queryConflictVIN(pCvedNum);
-	
-	if(dupVINCheck.isIssue){
-		cancel = true;
-		showMessage = true;
-		comment(dupVINCheck.issueMessage.join("\n<br>"));
-	}
-}else{
-	var dupVINCheck = queryConflictVIN();
-	
-	if(dupVINCheck.isIssue){
-		cancel = true;
-		showMessage = true;
-		comment(dupVINCheck.issueMessage.join("\n<br>"));
-	}
-}
-
 //For Accela testing purposes
-//validateCapLP();
+/*
+validateCapLP();
+ */
