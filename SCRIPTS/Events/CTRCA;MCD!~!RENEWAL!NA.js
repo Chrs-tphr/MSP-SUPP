620c97bf2e8aac01ae2eea4dce2c108be32563fc
l3lacknight <35308970+l3lacknight@users.noreply.github.com>
2018-04-19 22:41:59 +0000

    TEST

--- SCRIPTS/Events/CTRCA;MCD!~!RENEWAL!NA.js
+++ SCRIPTS/Events/CTRCA;MCD!~!RENEWAL!NA.js
@@ -1,11 +1,18 @@
 var authCapId = getParentLicenseCapID(capId);
 
 convertRenewalToReal();
-//assessRenewalDecalFee();
-//
-//if(authCapId){
-//	assessRenewalLateFees(authCapId);
-//}
+
+logDebug("Before public user")
+if (!publicUser)
+{
+	logDebug("After PUBLIC USER")
+	assessRenewalDecalFee();
+	if(authCapId)
+		{
+		assessRenewalLateFees(authCapId);
+		logDebug("After LATE FEES")
+		}
+}
 //
 //if(feeExists("RENEWAL") == false){
 //	updateFee("RENEWAL","MCD_AUTH_RENEW","FINAL",1,"Y");
