7b26ed09bfd99c35eb155741a8704a230e4be61b
Christopher <cisensano@yahoo.com>
2018-06-12 12:33:45 -0700

    new function that gets all invoiced fee codes from the record and updated CTRCA that voids and invoices those fees.

--- SCRIPTS/Events/CTRCA;~!~!~!~.js
+++ SCRIPTS/Events/CTRCA;~!~!~!~.js
@@ -4,8 +4,12 @@ if(!publicUser && !matches(appTypeArray[2],"Application","Renewal")){
 
 if (AInfo["Dismiss Incomplete Application"] == "CHECKED")
 {
-
-updateAppStatus("INCOMPLETE DISMISSAL","This is the test for the dismissal records."); // Unique status and comments
-closeWorkflow();
-
+	
+	updateAppStatus("INCOMPLETE DISMISSAL","This is the test for the dismissal records."); // Unique status and comments
+	closeWorkflow();
+	var ifca = getInvoicedFeeCodes(capId);
+	for(i in ifca){
+		voidRemoveFees(ifca[i]);
+	}
+	
 }
