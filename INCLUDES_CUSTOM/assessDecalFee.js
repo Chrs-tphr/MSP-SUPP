d0170ee50fddd3cb1028845cc2d6350580eba49e
l3lacknight <35308970+l3lacknight@users.noreply.github.com>
2018-10-10 13:26:14 -0400

    AM-142  Added Logs to understand code.

--- INCLUDES_CUSTOM/assessDecalFee.js
+++ INCLUDES_CUSTOM/assessDecalFee.js
@@ -1,17 +1,23 @@
 function assessDecalFee() {
 	//Application for Authority Approval date
 	approvDate = getStatusDateinTaskHistory("Compliance Review", "Compliance Approved");
+	logDebug("approvDate: "+approvDate);
 
 	if (approvDate != null) {
 		var approvDateYear = 1900 + approvDate.getYear();
+		logDebug("approvDate: "+approvDate);
 		var approvJSDate = new Date((approvDate.getMonth() +1) + "/" + approvDate.getDay() + "/" + approvDateYear);
+		logDebug("approvDateYear: "+approvDateYear);
 	}
 	//Update Equipment List Approved - Update Fees date
 	updateFeesDate = getStatusDateinTaskHistory("Application Review", "Approved - Update Fees");
+	logDebug("updateFeesDate: "+updateFeesDate);
 	
 	if (updateFeesDate != null) {
 		var updateFeesYear = 1900 + updateFeesDate.getYear();
+		logDebug("updateFeesYear: "+updateFeesYear);
 		var updateFeesJSDate = new Date((updateFeesDate.getMonth() +1) + "/" + updateFeesDate.getDay() + "/" + updateFeesYear);
+		logDebug("updateFeesJSDate: "+updateFeesJSDate);
 	}
 	//Update Equipment List ASIT submit
 	/*
@@ -23,9 +29,13 @@ function assessDecalFee() {
 	//set $50 fee date range for Application for Authority
 	startJSDate = new Date("06/30/" + approvDateYear);
 	endJSDate = new Date("10/31/" + approvDateYear);
+	logDebug("startJSDate: "+startJSDate);
+	logDebug("endJSDate: "+endJSDate);
 	//set $50 fee date range for Update Equipment List
 	startJSDate2 = new Date("06/30/" + updateFeesYear);
 	endJSDate2 = new Date("10/31/" + updateFeesYear);
+	logDebug("startJSDate2: "+startJSDate2);
+	logDebug("endJSDate2: "+endJSDate2);
 	//set $50 fee date range for Update Equipment List ACA
 	/*
 	startJSDate3 = new Date("06/30/" + todayDateYear);
@@ -37,14 +47,18 @@ function assessDecalFee() {
 	if ((approvJSDate > startJSDate && approvJSDate < endJSDate) || (updateFeesJSDate > startJSDate2 && updateFeesJSDate < endJSDate2)) {
 		feeAmt = ( sumASITColumn(equipTable, "Plate Fee", "EXCLUDE", "Equipment Use", "Household Goods", "INCLUDE", "Vehicle Action", "Add Vehicle") * 0.5 ) + 
 			sumASITColumn(equipTable, "Plate Fee", "INCLUDE", "Equipment Use", "Household Goods", "INCLUDE", "Vehicle Action", "Add Vehicle");
+		logDebug("If Statement Fee Amount: "+feeAmt);
 	}
 	else {
 		feeAmt = sumASITColumn(equipTable, "Plate Fee", "INCLUDE", "Vehicle Action", "Add Vehicle");
+		logDebug("Else Statement Fee Amount: "+feeAmt);
 	}
 	if ((feeAmt > 0) && (approvDate != null)) {
 		updateFee("DECAL", "MCD_AUTH_APP", "FINAL", feeAmt, "N");
+		logDebug("If Statement Update Fee #1: "+updateFee);
 	}
 	if ((feeAmt > 0) && (updateFeesDate != null)) {
 		updateFee("DECAL", "MCD_EQUIP", "FINAL", feeAmt, "N");
+		logDebug("If Statement Update Fee #2: "+updateFee);
 	}
 }
