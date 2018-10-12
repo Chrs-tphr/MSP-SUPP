dbdc6047664066004ca0ea459397bbe5a06d7a6b
Christopher <cisensano@yahoo.com>
2018-10-12 12:02:38 -1000

    fixed logDebug ()

--- SCRIPTS/Events/WTUA;MCD!~!APPLICATION!NA.js
+++ SCRIPTS/Events/WTUA;MCD!~!APPLICATION!NA.js
@@ -7,23 +7,14 @@ if(wfTask == "Application Review" && matches(wfStatus,"Accepted","Incomplete Not
 if((wfTask == "Compliance Review" && wfStatus == "Compliance Approved") || (wfTask == "Certification" && wfStatus == "Approved/Fees Due")){
 	assessDecalFee();
 	
-	if(wfDate != null)
-		{
-		logDebug ("wfDate = " +wfDate);
-		//wfDate = new Date(wfDate);
-		var	wfDate = new Date('2018-10-12');
+	if(wfDate != null){
+		logDebug("wfDate = " +wfDate);
+		var wfJsDate = new Date(wfDate);
 		var monthNum = wfDate.getMonth() + 1;
-			//var monthNum = wfDate.getMonth() +1;
-			
-			
-			
-			
-			logDebug ("monthNum = " +monthNum);
-		}
-	else
-		{
-		logDebug ("WF DATE IS NULL");
-		}
+		logDebug("monthNum = " +monthNum);
+	}else{
+		logDebug("WF DATE IS NULL");
+	}
 	if(matches(monthNum,10,11,12)){
 		var anyrdf = assessNextYearRenewalDecalFees();
 		if(anyrdf){
