35c09bd717c4d59d1462080d9b1eb90ea3ddeaa9
l3lacknight <35308970+l3lacknight@users.noreply.github.com>
2018-10-16 15:11:51 -0400

    Added a few log messages.

--- SCRIPTS/Events/WTUA;MCD!~!APPLICATION!NA.js
+++ SCRIPTS/Events/WTUA;MCD!~!APPLICATION!NA.js
@@ -7,7 +7,9 @@ if(wfTask == "Application Review" && matches(wfStatus,"Accepted","Incomplete Not
 if((wfTask == "Compliance Review" && wfStatus == "Compliance Approved") || (wfTask == "Certification" && wfStatus == "Approved/Fees Due")){
 	assessDecalFee();
 	
+	logDebug("wfDateMMDDYYYY = " +wfDateMMDDYYYY);
 	var wfsm = new Date(wfDateMMDDYYYY).getMonth()+1;
+	logDebug("wfsm value: " +wfsm);
 	if(matches(wfsm,10,11,12)){
 		var anyrdf = assessNextYearRenewalDecalFees();
 		if(anyrdf){
