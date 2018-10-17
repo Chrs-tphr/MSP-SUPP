88ecc667593c85286adb153e43c49242eac79df3
l3lacknight <35308970+l3lacknight@users.noreply.github.com>
2018-10-17 09:33:26 -0400

    Reverted AssessDecalFee back to exactly what is in production to check some things.

--- INCLUDES_CUSTOM/assessDecalFee.js
+++ INCLUDES_CUSTOM/assessDecalFee.js
@@ -1,61 +1,50 @@
-function assessDecalFee(){
+function assessDecalFee() {
 	//Application for Authority Approval date
 	approvDate = getStatusDateinTaskHistory("Compliance Review", "Compliance Approved");
-	logDebug("approvDate: "+approvDate);
-	var approvDateYear = null;
-	var updateFeesYear = null;
-	var approvJSDate = null;
-	var updateFeesJSDate = null;
 
-	if(approvDate != null){
-		approvDateYear = 1900 + approvDate.getYear();
-		logDebug("approvDateYear: "+approvDateYear);
-		approvJSDate = new Date((approvDate.getMonth() +1) + "/" + approvDate.getDay() + "/" + approvDateYear);
+	if (approvDate != null) {
+		var approvDateYear = 1900 + approvDate.getYear();
+		var approvJSDate = new Date((approvDate.getMonth() +1) + "/" + approvDate.getDay() + "/" + approvDateYear);
 	}
 	//Update Equipment List Approved - Update Fees date
 	updateFeesDate = getStatusDateinTaskHistory("Application Review", "Approved - Update Fees");
-	logDebug("updateFeesDate: "+updateFeesDate);
 	
-	if(updateFeesDate != null){
-		updateFeesYear = 1900 + updateFeesDate.getYear();
-		logDebug("updateFeesYear: "+updateFeesYear);
-		
-	    updateFeesJSDate = new Date((updateFeesDate.getMonth() +1) + "/" + updateFeesDate.getDay() + "/" + updateFeesYear);
-		logDebug("updateFeesJSDate: "+updateFeesJSDate);
+	if (updateFeesDate != null) {
+		var updateFeesYear = 1900 + updateFeesDate.getYear();
+		var updateFeesJSDate = new Date((updateFeesDate.getMonth() +1) + "/" + updateFeesDate.getDay() + "/" + updateFeesYear);
 	}
+	//Update Equipment List ASIT submit
+	/*
+	if (todayDate != null) {
+		var todayDateYear = 1900 + todayDate.getYear();
+		var todayDateJSDate = new Date((todayDate.getMonth() +1) + "/" + todayDate.getDay() + "/" + todayDateYear);
+	}
+	*/
 	//set $50 fee date range for Application for Authority
-	if(approvDate != null){
-		startJSDate = new Date("06/30/" + approvDateYear);
-		endJSDate = new Date("10/31/" + approvDateYear);
-		logDebug("startJSDate: "+startJSDate);
-		logDebug("endJSDate: "+endJSDate);
-	}
-	
+	startJSDate = new Date("06/30/" + approvDateYear);
+	endJSDate = new Date("10/31/" + approvDateYear);
 	//set $50 fee date range for Update Equipment List
-	if(updateFeesDate != null){
-		startJSDate2 = new Date("06/30/" + updateFeesYear);
-		endJSDate2 = new Date("10/31/" + updateFeesYear);
-		logDebug("startJSDate2: "+startJSDate2);
-		logDebug("endJSDate2: "+endJSDate2);
-	}
-	
+	startJSDate2 = new Date("06/30/" + updateFeesYear);
+	endJSDate2 = new Date("10/31/" + updateFeesYear);
+	//set $50 fee date range for Update Equipment List ACA
+	/*
+	startJSDate3 = new Date("06/30/" + todayDateYear);
+	endJSDate3 = new Date("10/31/" + todayDateYear);
+	*/
 	feeAmt = 0;
 	equipTable = loadASITable("EQUIPMENT LIST");
 
-	if((approvJSDate > startJSDate && approvJSDate < endJSDate) || (updateFeesJSDate > startJSDate2 && updateFeesJSDate < endJSDate2)){
+	if ((approvJSDate > startJSDate && approvJSDate < endJSDate) || (updateFeesJSDate > startJSDate2 && updateFeesJSDate < endJSDate2)) {
 		feeAmt = ( sumASITColumn(equipTable, "Plate Fee", "EXCLUDE", "Equipment Use", "Household Goods", "INCLUDE", "Vehicle Action", "Add Vehicle") * 0.5 ) + 
 			sumASITColumn(equipTable, "Plate Fee", "INCLUDE", "Equipment Use", "Household Goods", "INCLUDE", "Vehicle Action", "Add Vehicle");
-		logDebug("Half Year Decal Fee Amount: "+feeAmt);
-	}else{
-		feeAmt = sumASITColumn(equipTable, "Plate Fee", "INCLUDE", "Vehicle Action", "Add Vehicle");
-		logDebug("Full Year Decal Fee Amount: "+feeAmt);
 	}
-	if((feeAmt > 0) && (approvDate != null)){
-		logDebug("Updating Application Decal Fee: "+feeAmt);
+	else {
+		feeAmt = sumASITColumn(equipTable, "Plate Fee", "INCLUDE", "Vehicle Action", "Add Vehicle");
+	}
+	if ((feeAmt > 0) && (approvDate != null)) {
 		updateFee("DECAL", "MCD_AUTH_APP", "FINAL", feeAmt, "N");
 	}
 	if ((feeAmt > 0) && (updateFeesDate != null)) {
-		logDebug("Updating Equipment List Decal Fee: "+feeAmt);
 		updateFee("DECAL", "MCD_EQUIP", "FINAL", feeAmt, "N");
 	}
 }
